77825ca7cc4117acdfb5f6f13ce6e1b6ccbec39c
sandeepjayasu <sandeepbandarajayasundara@gmail.com>
2024-12-22 18:02:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "N1AwmCAJ#whkx_JnyqyNZ8MCLIiV1mZUnx5Ckbbj_sUOGW4hC5_E", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 AUTO_VOICE:"true" //true or false
